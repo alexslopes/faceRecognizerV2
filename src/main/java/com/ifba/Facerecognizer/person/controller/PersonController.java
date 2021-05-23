4a138414ed6167b37e034378fc94ee066a853703
alexslopes <alexstlopes@gmail.com>
2021-05-23 20:49:33 -0300

    Alterado Post para Get

--- src/main/java/com/ifba/Facerecognizer/person/controller/PersonController.java
+++ src/main/java/com/ifba/Facerecognizer/person/controller/PersonController.java
@@ -2,10 +2,7 @@ package com.ifba.Facerecognizer.person.controller;
 
 import com.ifba.Facerecognizer.person.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -27,7 +24,7 @@ public class PersonController {
         return("Recebi as imagens");
     }
 
-    @PostMapping("recognize")
+    @GetMapping("recognize")
     public List<String> recognizeFace(@RequestParam("images") MultipartFile[] images) {
         return personService.recognizePeople(images);
     }
